e477b70fa5a0f7d480a2b438944c3e0a6d704ab1
Anderson Alex Durante <anderson.alexdurante@gmail.com>
2020-05-16 19:19:18 -0300

    Cria servidor com express e adiciona knexfile

---
+++ src/routes.js
@@ -0,0 +1,5 @@
+const express = require('express')
+const routes = express.Router()
+
+const IceCreamController = require('./controllers/IceCreamController')
+const ProfileController = require('./controllers/ProfileController')
