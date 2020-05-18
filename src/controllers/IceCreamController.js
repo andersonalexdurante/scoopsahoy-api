802baec975670c124e2a865c75f3186ecfaff81f
Anderson Alex Durante <anderson.alexdurante@gmail.com>
2020-05-18 11:59:39 -0300

    Trata erros com status code rest

--- src/controllers/IceCreamController.js
+++ src/controllers/IceCreamController.js
@@ -2,56 +2,69 @@ const connection = require('../database/connection')
 
 module.exports = {
 
-    async list(req, res) {
-        const type = req.headers.type
+    async index(req, res, next) {
+        try {
+            const type = req.headers.type
 
-        //Mais recentes
-        if(type === 1) {
+            //Mais recentes
+            if(type === 1) {
+                const sorvetes = await connection('sorvetes')
+                .select('*').orderByRaw('id DESC').limit(20)
+
+                return res.json(sorvetes)
+            }
+            
+            //Mais populares
+            if(type === 2) {
+                const sorvetes = await connection('sorvetes')
+                .select('*').orderByRaw('favoritos DESC').limit(10)
+
+                return res.json(sorvetes)
+            }
+
+            //Aleatório
             const sorvetes = await connection('sorvetes')
-            .select('*').orderByRaw('id DESC').limit(20)
+            .select('*')
+            .orderByRaw('RANDOM()')
+            .limit(5)
+
+            sorvetes.sort(() => Math.random() - 0.5)
 
             return res.json(sorvetes)
+        } catch (error) {
+            next(error)
         }
         
-        //Mais populares
-        if(type === 2) {
-            const sorvetes = await connection('sorvetes')
-            .select('*').orderByRaw('favoritos DESC').limit(10)
+    },
 
-            return res.json(sorvetes)
+    async create(req, res, next) {
+        try {
+            const { nome, descricao, preco, favoritos } = req.body
+
+            const [ id ] = await connection('sorvetes')
+            .insert({
+                nome, 
+                descricao,
+                preco,
+                favoritos,
+            })
+    
+            return res.json({ id })   
+        } catch (error) {
+            next(error)
         }
-
-        //Aleatório
-        const sorvetes = await connection('sorvetes')
-        .select('*')
-        .orderByRaw('RANDOM()')
-        .limit(5)
-
-        sorvetes.sort(() => Math.random() - 0.5)
-
-        return res.json(sorvetes)
     },
 
-    async create(req, res) {
-        const { nome, descricao, preco, favoritos } = req.body
+    async delete(req, res, next) {
+        try {
+            const { id } = req.params
 
-        const [ id ] = await connection('sorvetes')
-        .insert({
-            nome, 
-            descricao,
-            preco,
-            favoritos,
-        })
+            await connection('sorvetes').where('id', id).delete()
 
-        return res.json({ id })
-    },
-
-    async delete(req, res) {
-        const { id } = req.params
-
-        await connection('sorvetes').where('id', id).delete()
-
-        return res.status(204).send();
+            return res.status(204).send();
+        } catch (error) {
+            next(error)
+        }
     }
 
 }
--- src/index.js
+++ src/index.js
@@ -8,4 +8,9 @@ app.use(cors())
 app.use(express.json())
 app.use(routes)
 
+app.use((error, req, res, next) => {
+    res.status(error.status || 500)
+    res.json({error: error.message})
+})
+
 app.listen(3333)
